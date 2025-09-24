5276da0d84917f765263db0085a2ba455302e38e
Silanur-Tezcan <147698132+Silanur-Tezcan@users.noreply.github.com>
2025-09-24 17:25:25 +0300

    Update hafta2.py
    Görüntü işleme fonksiyonları eklendi: latte.jpg dosyası yüklendi ve RGB formatına dönüştürüldü. Görüntü üzerinde cv2.flip() fonksiyonu kullanılarak dikey, yatay ve çift eksenli dönüşümler uygulandı. Aynı dönüşümler manuel olarak NumPy dilimleriyle de gerçekleştirildi. Görüntüleri karşılaştırmak için matplotlib ile subplot görselleştirmeleri yapıldı. Ayrıca, görüntünün sağ alt çeyreği crop işlemiyle ayrıştırıldı. Son olarak, flip işlemini gerçekleştiren kendi fonksiyonumuz (my_flip) tanımlandı ve test edildi.

--- hafta2.py
+++ hafta2.py
@@ -21,9 +21,93 @@ my_rgb_img = cv2.cvtColor(my_img, cv2.COLOR_BGR2RGB)
 # döndürme ekseni:-1 = hem yatay hem dikey eksen
 
 
-my_rgb_vertical_img = cv2.flip(my_rgb_img, 0)     # Dikey çevir
-my_rgb_horizontal_img = cv2.flip(my_rgb_img, 1)   # Yatay çevir
-my_rgb_both_img = cv2.flip(my_rgb_img, -1)        # Hem yatay hem dikey
+# my_rgb_vertical_img = cv2.flip(my_rgb_img, 0)     # Dikey çevir
+# my_rgb_horizontal_img = cv2.flip(my_rgb_img, 1)   # Yatay çevir
+# my_rgb_both_img = cv2.flip(my_rgb_img, -1)        # Hem yatay hem dikey
+
+# fig= plt.figure(figsize=(10,10))
+# plt.subplot(2,2,1)
+# plt.imshow(my_rgb_img)
+# plt.title("orjinal")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,2)
+# plt.imshow(my_rgb_vertical_img)
+# plt.title("vertical")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,3)
+# plt.imshow(my_rgb_horizontal_img)
+# plt.title("hortizonal")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,4)
+# plt.imshow(my_rgb_both_img)
+# plt.title("both")
+# plt.axis("off")
+# plt.show() 
+
+# my_new_vertical_img= my_img[::-1,:,:]
+# plt.imshow(my_new_vertical_img)
+
+# # ters çeviriyor
+# my_new_hortizonal_img=my_rgb_img[:,::-1,:]   
+
+# plt.show()
+# # hem dikey hem yatay ters çevir
+# my_new_both_img= my_rgb_img[::-1,::-1,:]
+
+
+
+# fig= plt.figure(figsize=(10,10))
+# plt.subplot(2,2,1)
+# plt.imshow(my_rgb_img)
+# plt.title("orjinal")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,2)
+# plt.imshow(my_new_vertical_img)
+# plt.title("vertical")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,3)
+# plt.imshow(my_new_hortizonal_img)
+# plt.title("hortizonal")
+# plt.axis("off")
+
+
+# plt.subplot(2,2,4)
+# plt.imshow(my_rgb_both_img)
+# plt.title("both")
+# plt.axis("off")
+# plt.show() 
+
+# sıfırdan kendi fonksiyonumuzu yazıyorum
+# 1-) cv2.flip() fonskiyonu iki parametreli oldugu içinde tanımlarken iki parametre tanımlıyoruz
+# 2-) ilk parametre, dönüştürülmek istenen orjinal görüntü(numpy.ndarray),ilk parametre tipi numpy.ndarray, 2. parametre tipi int
+
+def my_flip(org_img,flip_number):
+ 
+    if flip_number==0:
+        org_img= org_img[::]
+    elif flip_number==1:
+        org_img=org_img[:,::-1,:]
+    elif flip_number==-1:
+        org_img=org_img[::-1,::-1,:]
+    else:
+        print("lütfen doğru bir parametre girin")
+    return org_img
+
+
+my_flip_0=my_flip(my_rgb_img,0)
+my_flip_1=my_flip(my_rgb_img,1)
+my_flip_2=my_flip(my_rgb_img,2)
+
 
 fig= plt.figure(figsize=(10,10))
 plt.subplot(2,2,1)
@@ -33,26 +117,43 @@ plt.axis("off")
 
 
 plt.subplot(2,2,2)
-plt.imshow(my_rgb_vertical_img)
+plt.imshow(my_flip_0)
 plt.title("vertical")
 plt.axis("off")
 
 
 plt.subplot(2,2,3)
-plt.imshow(my_rgb_horizontal_img)
+plt.imshow(my_flip_1)
 plt.title("hortizonal")
 plt.axis("off")
 
 
 plt.subplot(2,2,4)
-plt.imshow(my_rgb_both_img)
+plt.imshow(my_rgb_img)
 plt.title("both")
 plt.axis("off")
 plt.show() 
 
 
+# elinizdeki görüntünün sağ en alt tarafını gösteren kısmı parçala. crop ile 
+
+my_rgb_img.shape
+bilgi_satir=int(my_rgb_img.shape[0]*0.75)
+bilgi_sutun=int(my_rgb_img.shape[0]*0.75)
+
+new_crop_1=my_rgb_img[bilgi_satir:,bilgi_sutun:,:]
+
+plt.subplot(1,2,1)
+plt.imshow(my_rgb_img)
+plt.title("hortizonal")
+plt.axis("off")
 
 
+plt.subplot(1,2,2)
+plt.imshow(new_crop_1)
+plt.title("both")
+plt.axis("off")
+plt.show() 
 
 
 
